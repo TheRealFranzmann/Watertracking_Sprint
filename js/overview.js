993c96800aec6f9a06752f89c9bab9ad6bf17fb0
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2023-01-24 10:21:43 +0100

    made js scripts work and added overview.js

---
+++ js/overview.js
@@ -0,0 +1,18 @@
+/*
+created by Kathrin Hubmann
+24.01.2023
+*/
+
+// variables
+var backBtn = document.getElementById('backBtn')
+
+// functions
+
+
+// stuff to happen
+
+
+// listeners
+backBtn.addEventListener('click', event => {
+    window.location.href = 'Watertracking.html';
+});
