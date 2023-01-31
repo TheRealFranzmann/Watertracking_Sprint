e2976756f5258e8b3ef12a4b42510146ca51866e
Benjamin Lamprecht <benjamin.lamprecht1@edu.fh-joanneum.at>
2023-01-31 12:56:14 +0100

    for Harry

--- database.js
+++ database.js
@@ -13,7 +13,7 @@ console.log('connected to watertracking');
 
 // if table not exists
 connection.query(
-    'CREATE TABLE IF NOT EXISTS wassertrinken (date DATE NOT NULL, glas INT , krug INT, PRIMARY KEY(date))',
+    'CREATE TABLE IF NOT EXISTS wassertrinken (id INT NOT NULL AUTO_INCREMENT , creationDate DATE NOT NULL , editDate DATE NOT NULL , name VARCHAR(255) NOT NULL , amount INT NOT NULL , unit VARCHAR(255) NOT NULL , PRIMARY KEY(id))',
     function (err, results, fields) {
         console.log(results);
     }
@@ -22,15 +22,15 @@ connection.query(
 // post dummy data
 function initDummyData() {
     connection.query(
-        'INSERT INTO wassertrinken (date, glas, krug)' + 
-                    'VALUES' +
-                    "('0030-01-01', 0, 1)," +
-                    "('0030-01-02', 2, 3)," +
-                    "('0030-01-03', 4, 5)," +
-                    "('0030-01-04', 6, 7)," +
-                    "('0030-01-05', 8, 9)," +
-                    "('0030-01-06', 10,11)," +
-                    "('0030-01-07', 12,13)",
+        'INSERT INTO wassertrinken (id, creationDate, editDate, name, amount, unit)' + 
+                    'VALUES' + "",
+                    // "('0030-01-01', 0, 1)," +
+                    // "('0030-01-02', 2, 3)," +
+                    // "('0030-01-03', 4, 5)," +
+                    // "('0030-01-04', 6, 7)," +
+                    // "('0030-01-05', 8, 9)," +
+                    // "('0030-01-06', 10,11)," +
+                    // "('0030-01-07', 12,13)",
         function (err, results, fields) {
             console.log(results);
         }
