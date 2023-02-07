d00a8b16cb370ed84d0c3bb39c2793dfb4142820
Bence Gacs <bence.gacs@gmail.com>
2023-02-07 02:12:22 +0100

    Change fetch type

--- public/js/detailed_overview.js
+++ public/js/detailed_overview.js
@@ -2,26 +2,23 @@
 created Bence Gacs
 24.01.2023
 */
-import { getContainerById } from "./database.js";
-
 var urlParams = new URLSearchParams(window.location.search);
 var id = urlParams.get("id");
 
-const rows = await fetch('http://localhost:5000/data').then(response => response.json()).then(containers => {
-    for (var i = 0; i <= containers.length - 1; i++) {
-        if (containers[i].id == id) {
-            container = containers[i];
+var container = fetch('http://localhost:5000/dataById/' + id)
+    .then(response => response.json())
+    .then(
+        container => {
             document.getElementById("containerName").innerHTML = container.name;
             document.getElementById("creationDate").innerHTML = container.creationDate;
             document.getElementById("editDate").innerHTML = container.editDate;
             document.getElementById("containerCapacity").innerHTML = container.amount;
             document.getElementById("unit").innerHTML = container.unit;
+        },
+        error => {
+            console.log('ERROR');
         }
-    };
-},
-error => {
-    console.log('ERROR');
-});
+    );
 
 // listeners
 backBtn.addEventListener('click', event => {
